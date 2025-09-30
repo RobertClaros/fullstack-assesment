786136a310cc5cf6719bd2b9db1dc022a6161dd3
Robert Claros <audioprobolivia@gmail.com>
2025-09-30 13:21:10 -0400

    FEAT(backend): Base project initialized. Added Spring Boot structure, dependencies (JPA, Mongo, Security, Web), and configured .gitignore.

---
+++ backend/_finconecta-api/finconecta-api/src/main/java/com/finconecta/robert/finconecta_api/FinconectaApiApplication.java
@@ -0,0 +1,13 @@
+package com.finconecta.robert.finconecta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinconectaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinconectaApiApplication.class, args);
+	}
+
+}
---
+++ backend/_finconecta-api/finconecta-api/src/test/java/com/finconecta/robert/finconecta_api/FinconectaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finconecta.robert.finconecta_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinconectaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
